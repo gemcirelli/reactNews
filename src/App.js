232e3308ad869d35c3599cf2f70e46e3492fac0a
Gabriel <gemcirelli@gmail.com>
2018-09-13 15:38:11 -0400

    Split components

--- src/App.js
+++ src/App.js
@@ -68,55 +68,87 @@ class App extends Component {
     return(
      
         <div className="App">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+          <Table
+            list={list}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
           
-          <form>
-            <input type="text"
-                  value={searchTerm}
-                  onChange={this.onSearchChange}
-                
-            /> 
-          </form>
-
-          {list.filter(isSearched(searchTerm)).map(item=>
-          
-              <div key={item.objectID}>
-               
-                <ul>
-                  <li>
-                      <a href={item.url}> {item.title} </a>
-                  </li>
-                  <li>
-                     {item.author}
-                  </li>
-                  <li>
-                    {item.num_comments} 
-                  </li>
-
-                  <li>
-                  {item.points}
-                  </li>
-
-                  <li>
-                    <button 
-                        onClick={()=>this.onDismiss(item.objectID)}
-                        type="button"
-                    >
-                          Dismiss
-                    </button>
-                  </li>
-                </ul>
-                
-     
-              </div>
-          )}
-
-
+          />
 
         </div>
 
-    
     );
+
+  }    
+
+}
+
+class Search extends Component{
+  render(){
+    const{value,onChange,children}=this.props;
+    return(
+        <form>
+          {children}
+          <input
+            type="text"
+            value={value}
+            onChange={onChange}
+          />
+        </form>
+    )
   }
 }
+
+class Table extends Component{
+  render(){
+    const{list,pattern, onDismiss}=this.props;
+    return(
+        <div>
+          {list.filter(isSearched(pattern)).map(item=>
+              
+            <div key={item.objectID}>
+            
+              <ul>
+                <li>
+                    <a href={item.url}> {item.title} </a>
+                </li>
+                <li>
+                  {item.author}
+                </li>
+                <li>
+                  {item.num_comments} 
+                </li>
+
+                <li>
+                {item.points}
+                </li>
+
+                <li>
+                  <button 
+                      onClick={()=>onDismiss(item.objectID)}
+                      type="button"
+                  >
+                        Dismiss
+                  </button>
+                </li>
+              </ul>
+    
+            </div>
+          )}
+
+        </div>
+
+
+    )
+  }
+}
+
+
 
 export default App;
