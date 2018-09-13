f8a84a2c599514dd09f8be6ea7959a429e1b6932
Gabriel <gemcirelli@gmail.com>
2018-09-13 14:35:21 -0400

    colocando todo tipo ES6 destructuring y controlled CH2

--- src/App.js
+++ src/App.js
@@ -64,21 +64,21 @@ class App extends Component {
 
  
   render() {
-  
+    const {list,searchTerm}= this.state;
     return(
      
         <div className="App">
-
+          
           <form>
             <input type="text"
-                  type="text"
+                  value={searchTerm}
                   onChange={this.onSearchChange}
                 
             /> 
           </form>
 
-
-          {this.state.list.filter(isSearched(this.state.searchTerm)).map(item=>
+          {list.filter(isSearched(searchTerm)).map(item=>
+          
               <div key={item.objectID}>
                
                 <ul>
